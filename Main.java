12ba15890cd87a2214f7aca6f78984977fc0e7bd
Alona1212 <139923603+Alona1212@users.noreply.github.com>
2023-08-27 19:14:02 +0200

    Add files via upload
    hw

---
+++ Main.java
@@ -0,0 +1,42 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        char myChar = 'G';
+        int myInt = 89;
+        byte myByte = 4;
+        short myShort = 56;
+        float myFloat = 4.7333436F;
+        double myDouble = 4.355453532;
+        long myLong = 12121;
+
+        System.out.println("char="+myChar);
+        System.out.println ("int="+myInt);
+        System.out.println ("byte="+myByte);
+        System.out.println ("short="+myShort);
+        System.out.println ("float="+myFloat);
+        System.out.println ("double="+myDouble);
+        System.out.println ("long="+myLong);
+
+        Character objectChar = 'G';
+        System.out.println("Character="+objectChar);
+
+
+        Integer objectInt = 89;
+        System.out.println("Integer="+objectInt);
+
+        Byte objectByte = 4;
+        System.out.println("Byte="+objectByte);
+
+        Short objectShort = 56;
+        System.out.println("Short="+objectShort);
+
+        Float objectFloat = 4.7333436F;
+        System.out.println("Float="+objectFloat);
+
+        Double objectDouble = 4.3555453532;
+        System.out.println("Double="+objectDouble);
+
+        Long objectLong = 12121L;
+        System.out.println("Long="+objectLong);
+    }}
